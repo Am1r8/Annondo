a7613af7478d687dd2143efd2df736c6ce4f5910
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2022-09-17 18:05:52 -0400

    test

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,8 @@ void main() async{
         '/school_log': (context) => const loginApp12(),
         '/TSS_school': (context) => const thornhill_inf(),
         '/teachersAuth': (context) => const TeacherLoginAUth(),
-        '/changeAuthAnnouncement': (context) => const ChangeDatabase(),
+        '/changeAuthAnnouncement': (context) => const ChangeDatabase()
       }
     )
   );
 }
-
